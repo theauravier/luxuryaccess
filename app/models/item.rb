c5f4d5646524b858af12b1b3b040ebba54650687
Gary Benezat <gbenezat@gmail.com>
2015-11-03 16:47:24 +0100

    basic stuff modified

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
 
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>", card: "345x230" }
+    styles: { medium: "1000x1000>", thumb: "100x100>", card: "345x230", add: "100x500" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/

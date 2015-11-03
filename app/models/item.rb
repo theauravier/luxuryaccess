119e7a06e4bd6efa638d164506f835e9b6823b5c
Theau Ravier <ravier.t@gmail.com>
2015-11-03 11:51:54 +0100

    Added photo upload for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
+
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
+
   belongs_to :owner, class_name: "User"
   has_many :bookings, dependent: :destroy
 
@@ -8,11 +16,5 @@ class Item < ActiveRecord::Base
   validates :daily_price, presence: :true
   validates :brand, presence: :true
   validates :city, presence: :true
-
-  has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
-
-  validates_attachment_content_type :picture,
-    content_type: /\Aimage\/.*\z/
 
 end

96586130060f968b5312fcd2f0bde51048b16fb5
Theau Ravier <ravier.t@gmail.com>
2015-11-04 15:31:45 +0100

    created simple home

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get 'pages/index' => 'pages#index'
 
   root 'items#index'
 

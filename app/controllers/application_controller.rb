8cc0587ce27ea3d01d9658d9742633813c131cf0
thibault finot <thibault.finot@gmail.com>
2015-11-06 14:14:10 +0100

    change the application controller mais ne marche pas, tjs pas d'enregistrement de first_name & last_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+  end
 end
